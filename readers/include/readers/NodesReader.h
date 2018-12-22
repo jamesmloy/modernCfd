db19478a57509e097a33c417cbc02882c73103d8
jamesmloy <jamesmadiganloy@gmail.com>
2018-12-22 10:04:45 -0600

    read node coordinates from file and store them in NodesReader. added << operator

--- readers/include/readers/NodesReader.h
+++ readers/include/readers/NodesReader.h
@@ -3,15 +3,26 @@
 
 #include "readers/SectionReader.h"
 
+#include "blaze/math/StaticVector.h"
+#include "blaze/math/DynamicVector.h"
+
 
 class NodesReader : public SectionReader
 {
 public:
+  using Coord = blaze::StaticVector<double, 3, false>;
+  using CoordArr = blaze::DynamicVector<Coord, false>;
+
   NodesReader() : SectionReader("$Nodes", "$EndNodes")
   {}
 
   void readSection(std::ifstream &f) override;
   void accept(MeshBuilderVisitor &v) override;
+
+private:
+  friend std::ostream &operator<<(std::ostream &s, NodesReader const &n);
+
+  CoordArr _coords;
 };
 
 #endif
--- readers/src/NodesReader.cpp
+++ readers/src/NodesReader.cpp
@@ -1,12 +1,60 @@
-  #include "readers/NodesReader.h"
+#include "readers/NodesReader.h"
+
+#include "utils/PrintElements.h"
+#include "utils/ReadType.h"
+
+std::ostream &operator<<(std::ostream &s, NodesReader const &n)
+{
+  s << "NodesReader\nCoordinates:\n";
+  Utils::printElements(s, n._coords);
+  return s;
+}
 
 
-  void NodesReader::readSection(std::ifstream &f)
+void NodesReader::readSection(std::ifstream &f)
+{
+  using namespace Utils;
+
   {
-    std::cout << "nodes section\n";
-    scanSection(endLabel(), f);
+    // first line is header
+    std::string ln;
+    getline(f, ln);
   }
 
+  int const sumTot = readType<int>(f);
+  int const numVerts = readType<int>(f);
 
-  void NodesReader::accept(MeshBuilderVisitor &v)
-  {}
+  _coords.resize(numVerts);
+
+  int dummyI;
+  int nv(0);
+  int vId(0);
+
+  for (int i = 0; i != sumTot; ++i)
+  {
+    // tag
+    f >> dummyI;
+    // dimension
+    f >> dummyI;
+    // save parametric
+    f >> dummyI;
+
+    // number of vertices
+    nv = readType<int>(f);
+
+    for (int v = 0; v != nv; ++v)
+    {
+      vId = readType<int>(f);
+      Coord &pos = _coords[vId];
+      pos[0] = readType<double>(f);
+      pos[1] = readType<double>(f);
+      pos[2] = readType<double>(f);
+    }
+  }
+
+  scanSection(endLabel(), f);
+}
+
+
+void NodesReader::accept(MeshBuilderVisitor &v)
+{}
